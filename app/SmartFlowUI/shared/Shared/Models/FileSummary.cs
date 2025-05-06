eb128d559c4358b1b4cf068adda7bcc3b0218d93
Scott Rutz <scrutz@microsoft.com>
2025-05-06 11:49:48 -0500

    cp

--- app/SmartFlowUI/shared/Shared/Models/FileSummary.cs
+++ app/SmartFlowUI/shared/Shared/Models/FileSummary.cs
@@ -1,11 +1,4 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Shared.Models;
 
 public record FileSummary(string DataUrl, string FileName, string ContentType);
