0805b7dd60c980f44fe0139b03300ac970756074
Scott Rutz <scrutz@microsoft.com>
2025-09-26 14:40:00 -0500

    Remove backend and frontend source files
    Deleted all source files from both backend and frontend directories, including configuration, service, agent, extension, model, component, and page files. This change removes the application's implementation, likely in preparation for a major refactor or decommissioning.

--- app/SmartFlowUI/frontend/Models/AnswerResult.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-
-namespace ClientApp.Models;
-
-public readonly record struct AnswerResult<TRequest>(
-    bool IsSuccessful,
-    ApproachResponse? Response,
-    TRequest Request);
--- app/SmartFlowUI/frontend/Models/RatingSelection.cs
+++
@@ -1,5 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-
-namespace ClientApp.Models;
-
-public record RatingSelection(Guid MessageId, int Rating = 0);
